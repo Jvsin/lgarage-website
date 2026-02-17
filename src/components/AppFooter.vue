42ed322969bd23098b89b38335e7312c794bba5d
Jasin <piotr.jasinski2002@gmail.com>
2026-02-17 20:53:55 +0100

    footer ig and fb icons

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,11 +1,25 @@
 <template>
-  <v-footer class="footer" color="surface">
+  <v-footer class="footer" color="red-background">
     <v-container class="py-6 d-flex flex-column flex-md-row align-center justify-space-between">
-      <div>
+      <div class="text-center">
         <div class="text-h6 font-weight-bold">Gawrych Serwis&Detaling</div>
         <div class="text-body-2">Warsztat samochodowy i detailing</div>
+        <div class="mt-2 d-flex ga-2 justify-center">
+          <v-btn
+            aria-label="Facebook"
+            icon="mdi-facebook"
+            size="small"
+            variant="text"
+          />
+          <v-btn
+            aria-label="Instagram"
+            icon="mdi-instagram"
+            size="small"
+            variant="text"
+          />
+        </div>
       </div>
-      <div class="text-body-2 mt-4 mt-md-0">
+      <div class="text-body-2 mt-4 mt-md-0 text-center">
         <div>Telefon: 000 000 000</div>
         <div>Email: kontakt@lgarage.pl</div>
         <div>Godziny: Pon-Pt 08:00-17:00</div>
