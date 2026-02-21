ec7531609a08ff830d80df95e1ae6d7c970499af
Jasin <piotr.jasinski2002@gmail.com>
2026-02-21 18:45:52 +0100

    router fix - scroll

--- src/router/index.ts
+++ src/router/index.ts
@@ -12,6 +12,9 @@ import { routes } from 'vue-router/auto-routes'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
+  scrollBehavior (_to, _from, savedPosition) {
+    return savedPosition || { top: 0 }
+  },
 })
 
 // --- NOWA CZĘŚĆ: ZABEZPIECZENIE (GUARD) ---
@@ -21,7 +24,7 @@ function getCurrentUser () {
   return new Promise((resolve, reject) => {
     const removeListener = onAuthStateChanged(
       getAuth(),
-      (user) => {
+      user => {
         removeListener()
         resolve(user)
       },
